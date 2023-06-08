d9a54cebe10941a76997a52c01aa1dc016969087
akkramm00 <houssaine.elyaacoubi82@gmail.com>
2023-06-08 23:01:28 +0000

    add all modiff

--- index.php
+++ index.php
@@ -51,6 +51,17 @@ $membres = array(
  echo $membres[1][0]. ' a ' .$membres[1][1]. ' ans. Son mail est '.$membres[1][2]. '<br>';
  echo $membres[2][0]. ' a ' .$membres[2][1]. ' ans. Son mail est '.$membres[2][2]. '<br>';
 
+for($ligne= 0; $ligne<3; $ligne++){
+  $membre_no = $ligne+1;
+  echo 'Membre numéro '. $membre_no.'<br>';
+  echo '<ul>';
+  for($col = 0; $col< 3; $col++){
+    echo '<li>' . $membres[$ligne][$col]. '</li>';
+  }
+  echo '</ul>';
+}
+
+
 ?> 
 
   <!--
