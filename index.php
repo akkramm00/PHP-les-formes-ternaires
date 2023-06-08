002ac06bdbd3913e3f47a5dbc4a5deef2a53c5f7
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-08 21:27:50 +0000

    add all with foreach boucle

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <html>
   <head>
-    <title>PHP les formes ternaires </title>
+    <title>PHP : les tableaux</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php
+// Tableau numéroté :
+$prenoms = array('Pierre', 'Paul' , 'Jaques');// les prénoms sont séparées par des virgules .
+//Pour parcourir le tableau et afficher les elements 
+// on utilise la boucle 'for '.
+
+for($x = 0;$x <= 2 ; $x++){
+  echo $prenoms[$x].'<br/>';
+}
+?><br/><?php
+// avce la boucle 'foreach' :
+foreach($prenoms as $prenom){
+  echo $prenom.'<br>';
+}
+
+// les tableaux associatifs :
+$age = array (
+  'Pierre' => 24,
+  'Paul' => 22,
+  'Jaques' => 'Non renseigné'
+);
+
+// pour parcourir les tableaux assocaitifs ,la boucle "foreach" est la seule alternative :
+// => example :
+foreach($age as age){
+  echo $age."<br>";
+}
+
+?> 
 
   <!--
   This script places a badge on your repl's full-browser view back to your repl's cover
